a99baf4507b0a867099e116b762e59fbf5528e1d
ezequielRiveros <eriveros298@gmail.com>
2020-11-18 21:56:53 -0300

    adding changes to the project

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 const express =require('express')
 const app= express()
 
-app.listen(8083,'localhost')
+app.listen(3000,'localhost')
 console.log("dirname " + __dirname)
 app.get('/',(req,res)=>{
     res.sendFile(__dirname + '/views/index.html')
+})
+app.get('/login',(req,res)=>{
+    res.sendFile(__dirname + '/views/login.html')
+})
+app.get('/register',(req,res)=>{
+    res.sendFile(__dirname + '/views/register.html')
 })
 app.get('*',(req,res)=>{
     
